0e8609d967871439d46e5720be0852334b41494a
Kenneth J. Pronovici <pronovic@ieee.org>
2025-02-24 12:12:07 -0600

    Add a scripted command to drive the browser

--- src/auth0token/cli.py
+++ src/auth0token/cli.py
@@ -9,8 +9,16 @@ import psutil
 import uvicorn
 from click import ClickException
 from dotenv import load_dotenv
+from environs import env
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from auth0token.auth0 import PORT, SERVER, get_authorization_endpoint
+
+RENDER_DELAY = 5  # wait for up to 5 seconds for the login page to render
 
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]})
@@ -37,7 +45,8 @@ def auth0token() -> None:
 )
 def retrieve(wait_sec: int, env_file: str) -> None:
     """
-    Retrieve an Auth0 JWT access token for an application and API.
+    Retrieve an Auth0 JWT access token for an application and API, manually
+    interacting with Firefox to log in.
 
     This coordinates the OIDC Authorization Code flow, submitting the correct
     requests to Auth0 and handling the required callback interaction.  This is
@@ -137,3 +146,121 @@ def retrieve(wait_sec: int, env_file: str) -> None:
         bar.update(wait_sec)
 
     click.echo("Login process completed; check Firefox for the access token, and then quit")
+
+
+@auth0token.command()
+@click.option(
+    "--env",
+    "-e",
+    "env_file",
+    metavar="<env-file>",
+    default=".env",
+    help="Path to env file on disk, default '.env'",
+)
+def scripted(env_file: str) -> None:
+    """
+    Retrieve an Auth0 JWT access token for an application and API, scripting
+    the login process via selenium.
+
+    This coordinates the OIDC Authorization Code flow, submitting the correct
+    requests to Auth0 and handling the required callback interaction.  This is
+    done by starting an ephemeral uvicorn server to provide the callback
+    endpoint and a private Firefox browser to handle the web UI aspects of the
+    flow.
+
+    When the command completes, the token will be dumped to stdout.
+
+    Your Auth0 application (under Applications > Applications) must have the
+    following allowed redirect URL:
+
+        \b
+        http://localhost:35000/localtoken/callback
+
+    The environment file must contain the following variables:
+
+        \b
+        BASE_URI = "https://<tenant>.us.auth0.com"
+        AUDIENCE = "<api-audience>"
+        CLIENT_ID = "<client-id>"
+        CLIENT_SECRET = "<client-secret>"
+        LOGIN_USERNAME = "<login-username>"
+        LOGIN_PASSWORD = "<login-password>"
+
+    There are two optional variables:
+
+        \b
+        CONNECTION = "<database-connection-name>"
+        ORGANIZATION_ID = "<organization-id>"
+
+    If you have your client configured with an organization prompt, then the
+    CONNECTION variable must be omitted.  If you have your client configured
+    with exactly one authentication data source, then the CONNECTION variable
+    is optional.  Otherwise, specify the name of the authentication data source
+    you want to log in with.
+
+    If you have your client configured with an organization prompt, then you
+    can optionally provide ORGANIZATION_ID here so you don't have to enter the
+    organization name in the web flow every time.  Note that this is the Auth0
+    organization identifier, not the lower-case name that you would enter in the
+    web flow.
+
+    If you use "-" for the location of your envfile, then the expected variables
+    are assumed to be in your environment already and no envfile is sourced.
+    """
+    server_args = {
+        "host": SERVER,
+        "port": PORT,
+        "log_level": "error",
+        "limit_max_requests": 1,
+    }
+
+    if env_file != "-":
+        server_args["env_file"] = env_file
+        load_dotenv(env_file)
+
+    server = Process(
+        target=uvicorn.run,
+        args=["auth0token.api:APP"],
+        kwargs=server_args,
+        daemon=True,
+    )
+    server.start()
+    sleep(1)
+
+    login_username = env.str("LOGIN_USERNAME")
+    login_password = env.str("LOGIN_PASSWORD")
+
+    browser = webdriver.Firefox()
+    browser.get(get_authorization_endpoint())
+
+    def wait_by_id(identifier: str) -> WebElement:
+        WebDriverWait(browser, RENDER_DELAY).until(EC.presence_of_element_located((By.ID, identifier)))
+        return browser.find_element(By.ID, identifier)
+
+    def wait_by_name(name: str) -> WebElement:
+        WebDriverWait(browser, RENDER_DELAY).until(EC.presence_of_element_located((By.NAME, name)))
+        return browser.find_element(By.NAME, name)
+
+    def wait_by_xpath(xpath: str) -> WebElement:
+        WebDriverWait(browser, RENDER_DELAY).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        return browser.find_element(By.XPATH, xpath)
+
+    username = wait_by_id("username")
+    submit = wait_by_name("action")
+    username.click()
+    username.clear()
+    username.send_keys(login_username)
+    submit.click()
+
+    password = wait_by_id("password")
+    submit = wait_by_name("action")
+    password.click()
+    password.clear()
+    password.send_keys(login_password)
+    submit.click()
+
+    content = wait_by_xpath("//pre")
+    token = content.get_attribute("innerHTML")
+    click.echo(token)
+
+    browser.quit()
